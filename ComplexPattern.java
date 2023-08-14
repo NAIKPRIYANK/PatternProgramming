6356bd98fdfa55cf393053fad12ae88758f38ce1
Priyank Bhikaji Naik <104668352+NAIKPRIYANK@users.noreply.github.com>
2023-08-14 23:27:35 +0530

    Add files via upload

--- ComplexPattern.java
+++ ComplexPattern.java
@@ -2,7 +2,7 @@ package CoreJava;
 class ComplexPattern
 {
     public static void main(String [] a){
-        int n = 27;
+        int n = 13;
         for(int i = 0; i<n;i++){
             for(int j=0;j<n;j++){
                 if(i==j || i+j==(n-1) || i==0 || j==0 || i==n-1 || j==n-1 || i+j==(n-1)/2 || i-j==(n-1)/2 || j-i==(n-1)/2 || i+j==(n-1)+(n-1)/2){
